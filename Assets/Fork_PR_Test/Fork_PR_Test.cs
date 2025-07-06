e208ad38645127c3a7c253279a76053afb5a1661
Ji Won <jgw0929@gmail.com>
2025-07-07 03:14:20 +0900

    Fork_PR_Test
    Fork_PR_Test과정

---
+++ Assets/Fork_PR_Test/Fork_PR_Test.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fork_PR_Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Make in Child");
+    }
+}
